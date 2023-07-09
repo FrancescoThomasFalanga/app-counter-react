dbcccb6a22e3a01fe1a92a55a94e57309c2e1bb0
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-07-09 17:12:04 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -16,26 +16,48 @@ function Counter() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
-  function handlePreviousStep() {
-    if (step > 1) {
-      setStep((s) => s - 1);
-    }
-  }
+  // function handlePreviousStep() {
+  //   if (step > 1) {
+  //     setStep((s) => s - 1);
+  //   }
+  // }
 
-  function handleNextStep() {
-    setStep((s) => s + 1);
+  // function handleNextStep() {
+  //   setStep((s) => s + 1);
+  // }
+
+  function reset() {
+    setStep(1);
+    setCount(0);
   }
 
   return (
     <div className="flex">
       <div className="steps">
-        <button onClick={handlePreviousStep}>-</button>
+        <input
+          type="range"
+          min={1}
+          max={10}
+          value={step}
+          onChange={(s) => setStep(Number(s.target.value))}
+        />
+
+        <h2>{step}</h2>
+
+        {/* <button onClick={handlePreviousStep}>-</button>
         <h2>Step: {step}</h2>
-        <button onClick={handleNextStep}>+</button>
+        <button onClick={handleNextStep}>+</button> */}
       </div>
       <div className="counter">
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <h2>Count: {count}</h2>
+        {/* <h2>Count: {count}</h2> */}
+
+        <input
+          type="number"
+          value={count}
+          onChange={(c) => setCount(Number(c.target.value))}
+        />
+
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
 
@@ -49,6 +71,15 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+
+      <button
+        onClick={reset}
+        style={
+          step === 1 && count === 0 ? { display: "none" } : { display: "block" }
+        }
+      >
+        Reset
+      </button>
     </div>
   );
 }
